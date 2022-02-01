4ac88a717d9826375cacc5ad28c11a9bf448b209
Yosua Nicolaus <cryodawn257@gmail.com>
2022-02-01 07:54:48 +0700

    create all methods and variables

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,12 +1,76 @@
+import java.awt.Color;
+import java.awt.Graphics;
 import java.awt.event.ActionEvent;
-
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import javax.swing.Action;
 import javax.swing.JPanel;
+import javax.swing.plaf.DimensionUIResource;
+
+import java.util.Random;
+import java.util.Timer;
+import java.util.random.*;
 
 public class GamePanel extends JPanel implements Action {
 
+    static final int SCREEN_WIDTH = 600;
+    static final int SCREEN_HEIGHT = 600;
+    static final int UNIT_SIZE = 25;
+    static final int GAME_UNITS = (SCREEN_WIDTH*SCREEN_HEIGHT)/UNIT_SIZE;
+    static final int DELAY = 75;
+
+    final int x[] = new int[GAME_UNITS];
+    final int y[] = new int[GAME_UNITS];
+    int bodyParts = 6;
+    int applesEaten = 0;
+    int appleX;
+    int appleY;
+    char direction = 'R';
+    boolean running = false;
+    Timer timer;
+    Random random;
+
+
     GamePanel() {
+        random = new Random();
+        this.setPreferredSize(new DimensionUIResource(SCREEN_WIDTH, SCREEN_HEIGHT));
+        this.setBackground(Color.black);
+        this.setFocusable(true);
+        this.addKeyListener(new MyKeyAdapter());
+
+        startGame();
+    }
+
+    public void startGame() {
+
+    }
+
+    public void paintComponent(Graphics g) {
+
+    }
+
+    public void draw(Graphics g) {
+
+    }
+
+    public void newApple() {
         
+    }
+
+    public void move() {
+
+    }
+
+    public void checkApple() {
+
+    }
+
+    public void checkCollisions() {
+
+    }
+
+    public void gameOver(Graphics g) {
+
     }
 
     @Override
@@ -27,4 +91,10 @@ public class GamePanel extends JPanel implements Action {
         
     }
     
+    public class MyKeyAdapter extends KeyAdapter {
+        @Override
+        public void keyPressed(KeyEvent e) {
+
+        }
+    }
 }
