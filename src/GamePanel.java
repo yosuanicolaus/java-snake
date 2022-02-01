88cb940ae8baa85f8da374445601d047e6b609e2
Yosua Nicolaus <cryodawn257@gmail.com>
2022-02-01 07:40:27 +0700

    set basic boilerplate

---
+++ src/GamePanel.java
@@ -0,0 +1,30 @@
+import java.awt.event.ActionEvent;
+
+import javax.swing.Action;
+import javax.swing.JPanel;
+
+public class GamePanel extends JPanel implements Action {
+
+    GamePanel() {
+        
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public Object getValue(String key) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void putValue(String key, Object value) {
+        // TODO Auto-generated method stub
+        
+    }
+    
+}
